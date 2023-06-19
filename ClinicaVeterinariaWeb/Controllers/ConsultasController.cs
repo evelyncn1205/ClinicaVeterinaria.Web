b52ebab797d15985439f86da90840a2689435a31
Evelyncn1205 <evelynrx_rj@hotmail.com>
2023-06-19 14:23:18 +0100

    Init Project

--- ClinicaVeterinariaWeb/Controllers/ConsultasController.cs
+++ ClinicaVeterinariaWeb/Controllers/ConsultasController.cs
@@ -54,7 +54,7 @@ namespace ClinicaVeterinariaWeb.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ClientName,AnimalName,CellPhone,Date,Time,Doctor,ConsultationType,Note")] Consulta consulta)
+        public async Task<IActionResult> Create(Consulta consulta)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace ClinicaVeterinariaWeb.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ClientName,AnimalName,CellPhone,Date,Time,Doctor,ConsultationType,Note")] Consulta consulta)
+        public async Task<IActionResult> Edit(int id, Consulta consulta)
         {
             if (id != consulta.Id)
             {

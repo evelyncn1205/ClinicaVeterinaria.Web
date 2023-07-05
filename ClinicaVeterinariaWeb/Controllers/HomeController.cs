e4c83eb3cf6fbb4f355cd2e1e40a4e1b3d663916
Evelyncn1205 <evelynrx_rj@hotmail.com>
2023-07-05 11:52:47 +0100

    Add view about us

--- ClinicaVeterinariaWeb/Controllers/HomeController.cs
+++ ClinicaVeterinariaWeb/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ClinicaVeterinariaWeb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }

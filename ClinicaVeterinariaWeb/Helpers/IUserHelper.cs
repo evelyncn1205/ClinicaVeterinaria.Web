ca6b08fb9dc99e30ac19041a8034474b706c62da
Evelyncn1205 <evelynrx_rj@hotmail.com>
2023-09-11 20:23:51 +0100

    Recover and reset password

--- ClinicaVeterinariaWeb/Helpers/IUserHelper.cs
+++ ClinicaVeterinariaWeb/Helpers/IUserHelper.cs
@@ -29,5 +29,9 @@ namespace ClinicaVeterinariaWeb.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
     }
 }
